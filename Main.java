eb35c3a62981f2633b464a2ce3045c58c54b05a2
Eric Jefferson <ejefferson50@gmail.com>
2019-02-06 09:37:12 -0500

    Finished Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.nextLine();
+        if(name.equals("Alice")){
+            System.out.println("Hi " + name);
+        } else if(name.equals("Bob")){
+            System.out.println("Hi " + name);
+        } else{System.out.println("I am sorry but do I know you?");
+        }
+        
+            
+        
+        
+        
+        
+        
+        
 
     }
 }
